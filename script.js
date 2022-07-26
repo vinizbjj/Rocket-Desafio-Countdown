4149617125a99ece77a897780d4f3a518cbe2a6d
vinizbjj <84604002+vinizbjj@users.noreply.github.com>
2022-07-26 13:25:13 -0300

    Rocketseat Countdown

---
+++ script.js
@@ -0,0 +1,33 @@
+const daysEL = document.getElementById("days")
+const hoursEL = document.getElementById("hours")
+const minutesEL = document.getElementById("minutes")
+const secondsEL = document.getElementById("seconds")
+
+
+const newYears = "27 Jul 2022";
+
+function countdown() {
+
+    const newYearsDate = new Date(newYears);
+    const currentDate = new Date();
+
+    const totalSeconds = (newYearsDate - currentDate) / 1000;
+
+    const hours = Math.floor(totalSeconds / 3600 / 24);
+    const days = Math.floor(totalSeconds / 3600) % 24;
+    const minutes = Math.floor(totalSeconds / 60) % 60;
+    const seconds = Math.floor(totalSeconds) % 60;
+
+    daysEL.innerHTML = days;
+    hoursEL.innerHTML = formatTime(hours);
+    minutesEL.innerHTML = formatTime(minutes);
+    secondsEL.innerHTML = formatTime(seconds);
+
+}
+function formatTime(time) {
+    return time < 10 ? `0${time}` : time;
+
+}
+countdown();
+
+setInterval(countdown, 1000);
